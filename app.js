4ba4014397896772d865f748b1141ef5af786e31
Pedro Henrique <pedroh.tav.p@gmail.com>
2024-11-02 10:06:46 -0300

    Tentativa de addon de API no projeto

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
 
-const clearButton = document.getElementById('Button');
-
-clearButton.addEventListener('click', function(){
-    const form = document.getElementById('contato__form');
-    form.reset();
-});
